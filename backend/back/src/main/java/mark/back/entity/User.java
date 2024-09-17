c846daae27cc61c47f23a4ff4d35b3270b509a43
markoka101 <markoka101@gmail.com>
2024-09-17 16:18:30 -0400

    created fields for entity

---
+++ backend/back/src/main/java/mark/back/entity/User.java
@@ -0,0 +1,56 @@
+package mark.back.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "username cannot be blank")
+    @NonNull
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @NotBlank(message = "password cannot be blank")
+    @NonNull
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
