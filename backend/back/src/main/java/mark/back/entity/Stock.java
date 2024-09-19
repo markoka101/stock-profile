982181a6cc941c500fc47139ba2b37f21dbfce56
markoka101 <markoka101@gmail.com>
2024-09-19 15:47:11 -0400

    started stock entity

---
+++ backend/back/src/main/java/mark/back/entity/Stock.java
@@ -0,0 +1,44 @@
+package mark.back.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.Set;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "stocks")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column
+    private String symbol;
+
+    /*
+    Value will be in USD
+    Value will hold the last price found
+     */
+    @NonNull
+    @Column
+    private Float value;
+
+    /*
+    The date is the last time the price was updated
+     */
+    @NonNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
+    @Column
+    private Date date;
+
+    @ManyToMany(mappedBy = "stocksSaved")
+    private Set<User> saved;
+
+}
