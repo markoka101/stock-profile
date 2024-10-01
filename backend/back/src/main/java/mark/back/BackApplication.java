69c2c0df2f4e36a5de40911ec2428e6cb1f88eea
markoka101 <markoka101@gmail.com>
2024-10-01 15:35:32 -0400

    added bcrypt bean

--- backend/back/src/main/java/mark/back/BackApplication.java
+++ backend/back/src/main/java/mark/back/BackApplication.java
@@ -2,12 +2,19 @@ package mark.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
