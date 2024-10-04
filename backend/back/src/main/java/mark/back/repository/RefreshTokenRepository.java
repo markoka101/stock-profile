3004c7f153adb3d83ac333f624469ec315f9495a
markoka101 <markoka101@gmail.com>
2024-10-04 14:40:16 -0400

    find refresh token by user

--- backend/back/src/main/java/mark/back/repository/RefreshTokenRepository.java
+++ backend/back/src/main/java/mark/back/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package mark.back.repository;
 import mark.back.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,String> {
+    Optional<RefreshToken> findByUser(Long userId);
 }
