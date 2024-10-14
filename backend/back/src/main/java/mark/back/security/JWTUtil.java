a47e7248594dcfc7bf08ad19f560c5e990888b20
markoka101 <markoka101@gmail.com>
2024-10-14 14:56:43 -0400

    Started class for token util

---
+++ backend/back/src/main/java/mark/back/security/JWTUtil.java
@@ -0,0 +1,11 @@
+package mark.back.security;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+
+@Component
+public class JWTUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JWTUtil.class);
+}
