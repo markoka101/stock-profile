058d315750851189345280fbf8973bc619aab4be
markoka101 <markoka101@gmail.com>
2024-10-22 16:37:40 -0400

    added generate token method

--- backend/back/src/main/java/mark/back/security/JWTUtil.java
+++ backend/back/src/main/java/mark/back/security/JWTUtil.java
@@ -1,11 +1,28 @@
 package mark.back.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import mark.back.entity.User;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import org.slf4j.Logger;
 
+import java.util.Date;
+
 @Component
 public class JWTUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(JWTUtil.class);
+    static final String issuer = "spring-data";
+
+
+    //generate token
+    public String generateAccessToken(User user) {
+        return JWT.create()
+                .withIssuer(issuer)
+                .withSubject(String.format("%s,%s", user.getId(), user.getUsername()))
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstraints.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstraints.SECRET_KEY));
+    }
 }
