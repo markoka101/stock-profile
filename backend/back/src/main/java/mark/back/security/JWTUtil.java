858e2e9e948c958884a748c5abb68c46219e7bad
markoka101 <markoka101@gmail.com>
2024-10-29 17:27:58 -0400

    added function to generate refresh token

--- backend/back/src/main/java/mark/back/security/JWTUtil.java
+++ backend/back/src/main/java/mark/back/security/JWTUtil.java
@@ -2,6 +2,7 @@ package mark.back.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import mark.back.entity.RefreshToken;
 import mark.back.entity.User;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -24,5 +25,16 @@ public class JWTUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstraints.TOKEN_EXPIRATION))
                 .sign(Algorithm.HMAC512(SecurityConstraints.SECRET_KEY));
+    }
+
+    //generate refresh token
+    public String generateRefreshToken(User user, RefreshToken refreshToken) {
+        return JWT.create()
+                .withIssuer(issuer)
+                .withSubject(String.format("%s,%s", user.getId(), user.getUsername()))
+                .withClaim("tokenId", refreshToken.getToken())
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstraints.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstraints.REFRESH_KEY));
     }
 }
