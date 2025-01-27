b8b597f21b38790a2418be50b421275f1781f3c2
markoka101 <markoka101@gmail.com>
2025-01-27 15:50:42 -0500

    started working on JWT filter

---
+++ backend/back/src/main/java/mark/back/security/filter/JWTFilter.java
@@ -0,0 +1,35 @@
+package mark.back.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mark.back.security.JWTUtil;
+import mark.back.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private UserService userService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(JWTFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+
+        } catch (Exception e) {
+            LOGGER.error("Cannot set authentication",e);
+        }
+    }
+}
