8abb728c58919193b70c17552b05e915734ded40
markoka101 <markoka101@gmail.com>
2025-01-28 20:11:18 -0500

    created filter for jwt

--- backend/back/src/main/java/mark/back/security/filter/JWTFilter.java
+++ backend/back/src/main/java/mark/back/security/filter/JWTFilter.java
@@ -4,12 +4,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mark.back.entity.User;
 import mark.back.security.JWTUtil;
 import mark.back.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -22,14 +27,35 @@ public class JWTFilter extends OncePerRequestFilter {
     private JWTUtil jwtUtil;
     @Autowired
     private UserService userService;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(JWTFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
-
+            Optional<String> accessToken = parseAccessToken(request);
+            if (accessToken.isPresent() && jwtUtil.validateAccessToken(accessToken.get())) {
+                //grab user from the token
+                User user = userService.getUser(Long.parseLong(jwtUtil.getUserIdFromRefreshToken(accessToken.get())));
+                //create username and password auth token with user
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(token);
+            }
         } catch (Exception e) {
             LOGGER.error("Cannot set authentication",e);
         }
+
+        filterChain.doFilter(request,response);
+    }
+
+    //parse the access token
+    private Optional<String> parseAccessToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            return Optional.of(authHeader.replace("Bearer ", ""));
+        }
+
+        return Optional.empty();
     }
 }
