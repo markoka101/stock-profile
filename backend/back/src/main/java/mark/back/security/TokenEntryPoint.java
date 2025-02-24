ecddba5b88457da4e2f0a1a45f784c935e8ccf2a
markoka101 <markoka101@gmail.com>
2025-02-24 17:09:12 -0500

    implement class

--- backend/back/src/main/java/mark/back/security/TokenEntryPoint.java
+++ backend/back/src/main/java/mark/back/security/TokenEntryPoint.java
@@ -1,4 +1,26 @@
 package mark.back.security;
 
-public class TokenEntryPoint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class TokenEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException, ServletException {
+        LOGGER.error("unauthorized", authenticationException);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
