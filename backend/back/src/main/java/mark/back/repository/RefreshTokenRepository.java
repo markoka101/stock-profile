338ad5af970cb6f9d8ff1c4a0439043a3eb1db38
markoka101 <markoka101@gmail.com>
2025-02-24 17:07:59 -0500

    fixed oopsie

--- backend/back/src/main/java/mark/back/repository/RefreshTokenRepository.java
+++ backend/back/src/main/java/mark/back/repository/RefreshTokenRepository.java
@@ -1,10 +1,11 @@
 package mark.back.repository;
 
 import mark.back.entity.RefreshToken;
+import mark.back.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,String> {
-    Optional<RefreshToken> findByUser(Long userId);
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByUser(User user);
 }
