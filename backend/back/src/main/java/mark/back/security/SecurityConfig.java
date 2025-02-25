e4186db94ddd5758e6a3ebfa032684550384def3
markoka101 <markoka101@gmail.com>
2025-02-25 16:55:37 -0500

    security config

---
+++ backend/back/src/main/java/mark/back/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package mark.back.security;
+
+import lombok.AllArgsConstructor;
+import mark.back.security.filter.JWTFilter;
+import mark.back.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    private UserService userService;
+    private TokenEntryPoint tokenEntryPoint;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(
+                username -> userService.getUser(username)
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JWTFilter jwtFilter() {
+        return new JWTFilter();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, SecurityConstraints.REGISTER_PATH).permitAll()
+                        .requestMatchers(HttpMethod.POST, SecurityConstraints.LOGIN_PATH).permitAll()
+                        .requestMatchers(HttpMethod.POST, SecurityConstraints.LOGOUT_PATH).permitAll()
+                        .anyRequest().authenticated()).exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(tokenEntryPoint))
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+}
