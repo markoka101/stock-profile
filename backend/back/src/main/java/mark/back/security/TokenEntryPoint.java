c2502e5f31be98dcad02549ff684784db2a4da0a
markoka101 <markoka101@gmail.com>
2025-03-03 15:45:48 -0500

    token entry point

--- backend/back/src/main/java/mark/back/security/TokenEntryPoint.java
+++ backend/back/src/main/java/mark/back/security/TokenEntryPoint.java
@@ -20,7 +20,7 @@ public class TokenEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException, ServletException {
-        LOGGER.error("unauthorized", authenticationException);
+        LOGGER.error(authenticationException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
