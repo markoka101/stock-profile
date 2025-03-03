6ec59bf152868047eb0084ae449355a454254697
markoka101 <markoka101@gmail.com>
2025-03-03 15:48:12 -0500

    implemented service class and interface for refresh token

--- backend/back/src/main/java/mark/back/service/RefreshTokenService.java
+++ backend/back/src/main/java/mark/back/service/RefreshTokenService.java
@@ -1,4 +1,22 @@
 package mark.back.service;
 
+import mark.back.entity.RefreshToken;
+import mark.back.entity.User;
+
 public interface RefreshTokenService {
+    //create refresh token
+    RefreshToken createRefreshToken(User user);
+
+    //find refresh token by id
+    RefreshToken getRefreshToken(String id);
+
+    //save changes to refresh token
+    RefreshToken saveRefreshToken(RefreshToken refreshToken);
+
+
+    //check if refresh token with user if exists
+    boolean refreshTokenExists(User user);
+
+    //delete token by user
+    int deleteToken(User user);
 }
--- backend/back/src/main/java/mark/back/service/RefreshTokenServiceImpl.java
+++ backend/back/src/main/java/mark/back/service/RefreshTokenServiceImpl.java
@@ -1,4 +1,51 @@
 package mark.back.service;
 
-public class RefreshTokenServiceImpl {
+import lombok.AllArgsConstructor;
+import mark.back.entity.RefreshToken;
+import mark.back.entity.User;
+import mark.back.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@AllArgsConstructor
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+    RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    public RefreshToken createRefreshToken(User user) {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUser(user);
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    @Override
+    public RefreshToken saveRefreshToken(RefreshToken refreshToken) {
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+
+    @Override
+    public boolean refreshTokenExists(User user) {
+        return refreshTokenRepository.findByUser(user).isPresent();
+    }
+
+
+    @Override
+    @Transactional
+    public int deleteToken(User user) {
+        refreshTokenRepository.deleteByUser(user);
+
+        if (refreshTokenExists(user)) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+    @Override
+    public RefreshToken getRefreshToken(String id) {
+        return refreshTokenRepository.findById(id).isPresent() ? refreshTokenRepository.findById(id).get() : null;
+    }
 }
