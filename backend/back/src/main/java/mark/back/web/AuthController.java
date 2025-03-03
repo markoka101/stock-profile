bf226fe98b423e2b07bda45a979c86fe084f4e59
markoka101 <markoka101@gmail.com>
2025-03-03 15:49:03 -0500

    implement sign up, login, and logout api

--- backend/back/src/main/java/mark/back/web/AuthController.java
+++ backend/back/src/main/java/mark/back/web/AuthController.java
@@ -8,13 +8,19 @@ import mark.back.pojo.AuthRequest;
 import mark.back.pojo.AuthResponse;
 import mark.back.repository.RefreshTokenRepository;
 import mark.back.security.JWTUtil;
+import mark.back.service.RefreshTokenService;
 import mark.back.service.UserService;
 import org.postgresql.plugin.AuthenticationRequestType;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.IllegalTransactionStateException;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,7 +31,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AuthController {
     AuthenticationManager authenticationManager;
-    RefreshTokenRepository refreshTokenRepository;
+    RefreshTokenService refreshTokenService;
     UserService userService;
     JWTUtil jwtUtil;
 
@@ -40,16 +46,33 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@Valid @RequestBody AuthRequest authRequest) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         User user = userService.getUser((String) authentication.getPrincipal());
 
-        RefreshToken refreshTokenObj = new RefreshToken();
+        //this will delete previous refresh token if it already exists
+        refreshTokenService.deleteToken(user);
+        //create refresh token object
+        RefreshToken refreshTokenObj = refreshTokenService.createRefreshToken(user);
+
         //generate both access and refresh tokens
         String accessToken = jwtUtil.generateAccessToken(user);
         String refreshToken = jwtUtil.generateRefreshToken(user, refreshTokenObj);
 
+
         AuthResponse response = new AuthResponse(user.getId(), accessToken, refreshToken);
         return ResponseEntity.ok(response);
+    }
 
+    //sign out
+    @PostMapping("/signout")
+    public ResponseEntity<?> signOut(@RequestBody AuthResponse auth) {
+        String refreshToken = auth.getRefreshToken();
+        if (jwtUtil.validateRefreshToken(refreshToken) && refreshTokenService.getRefreshToken(jwtUtil.getTokenIdFromRefreshToken(refreshToken)) != null) {
+            refreshTokenService.deleteToken(userService.getUser(auth.getId()));
+            return ResponseEntity.ok().build();
+        }
+
+        throw new BadCredentialsException("invalid token");
     }
 
 
